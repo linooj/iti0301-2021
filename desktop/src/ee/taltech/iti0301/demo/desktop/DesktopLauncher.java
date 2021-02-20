56b7d07f365a5f1e87897b7010e054db16dfdac2
Liisi Nõojärv <linooj@ttu.ee>
2021-02-20 16:46:59 +0200

    first setup

---
+++ desktop/src/ee/taltech/iti0301/demo/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ee.taltech.iti0301.demo.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ee.taltech.iti0301.demo.MyGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyGdxGame(), config);
+	}
+}
